f791b24b6a70bdf6ca32215daee9b5a8a77233f5
Menzilith <100640967+Menzilith@users.noreply.github.com>
2025-09-03 10:30:15 -0300

    Update arquivo1.py

--- pasta1/arquivo1.py
+++ pasta1/arquivo1.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("olá mundo")
-=======
 print("Hello world")
->>>>>>> parent of 7eff9c9 (exclamação)
-=======
-print("Hello world")
->>>>>>> parent of 7eff9c9 (exclamação)
